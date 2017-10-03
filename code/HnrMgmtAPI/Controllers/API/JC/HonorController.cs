dfc7243540f317307cc8c0e00c68d041cd48e428
Terry-Ren <620461@qq.com>
2017-10-03 16:14:02 +0800

    2017.10.03 文件初始化

---
+++ code/HnrMgmtAPI/Controllers/API/JC/HonorController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using HnrMgmtAPI.Models;
+using System.Web.Http.Controllers;
+using System.Web;
+
+namespace HnrMgmtAPI.Controllers.API.JC
+{
+    [RoutePrefix("api/honor")]
+    public class HonorController : ApiController
+    {
+        ApiResult result = new ApiResult();
+
+        public ApiResult Get()
+        {
+            try
+            {
+                string value = HttpRuntime.Cache.Get("key").ToString();
+                result.status = "success";
+                result.messages = "读取Cache成功  key:" + value;
+
+                HttpRuntime.Cache.Remove("key");
+            }
+            catch
+            {
+                result.status = "error";
+                result.messages = "读取Cache失败  key:";
+            }
+
+            return result;
+        }
+
+        [HttpGet, Route("set")]
+        public ApiResult Set(string value)
+        {
+            ApiResult result = new ApiResult();
+
+            HttpRuntime.Cache.Insert("key", value);
+
+            result.status = "success";
+            result.messages = "设置Cache成功  key:" + value;
+
+            return result;
+        }
+    }
+}
---
+++ code/HnrMgmtAPI/Models/ApiResult.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace HnrMgmtAPI.Models
+{
+    public class ApiResult
+    {
+        /// <summary>
+        /// 返回结果success或error 表示此次访问状态
+        /// </summary>
+        public string status { get; set; }
+
+        /// <summary>
+        /// 失败原因
+        /// </summary>
+        public string messages { get; set; }
+
+        /// <summary>
+        /// 字段格式错误信息
+        /// </summary>
+        public Dictionary<string, string> fieldErrors { get; set; }
+
+        /// <summary>
+        /// 返回数据
+        /// </summary>
+        public object data { get; set; }
+    }
+}
