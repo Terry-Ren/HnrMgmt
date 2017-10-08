918b9d42e7844ef279ef7bc0103e6a725a705414
Wong Jianming <1249331134@qq.com>
2017-10-08 14:46:08 +0800

    2017.10.08 账户管理接口测试完成

--- code/HnrMgmtAPI/App_Start/RouteConfig.cs
+++ code/HnrMgmtAPI/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace HnrMgmtAPI
@@ -16,8 +12,7 @@ namespace HnrMgmtAPI
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                //defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-                defaults: new { controller = "Home", action = "ApiIndex", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
