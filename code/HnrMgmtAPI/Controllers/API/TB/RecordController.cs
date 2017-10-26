7819b296df429c10e9d0aa3282f317dbdf78dd9b
JianMingWang <1249331134@qq.com>
2017-10-26 08:57:58 +0800

    2017.10.26 添加填报模块

---
+++ code/HnrMgmtAPI/Controllers/API/TB/RecordController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using HnrMgmtAPI.Controllers.API;
+using HnrMgmtAPI.Models.API;
+using HnrMgmtAPI.Models.API.TB;
+using HnrMgmtAPI.Common;
+
+namespace HnrMgmtAPI.Controllers.API.TB
+{
+    [RoutePrefix("")]
+    public class RecordController : BaseApiController
+    {
+        #region 荣誉信息填报
+        [HttpPost, Route("")]
+        public ApiResult HnrRecord([FromBody]TB_HnrRecord hnrRecord)
+        {
+            result = AccessToken.Check(hnrRecord, "api/account/teacher");
+            return null;
+        }
+        #endregion
+
+        #region 竞赛获奖填报
+        [HttpPost, Route("")]
+        public ApiResult AwdRecord()
+        {
+            return null;
+        }
+        #endregion
+    }
+}
---
+++ code/HnrMgmtAPI/Models/API/TB/TB_HnrRecord.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HnrMgmtAPI.Models.API.TB
+{
+    public class TB_HnrRecord
+    {
+        public string access_token { get; set; }
+        public string HonorID { get; set; }
+        public string Year { get; set; }
+    }
+}
